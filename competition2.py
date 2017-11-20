137e826a11b51a5dde8408d0feb9130674aaadc4
Logan Allen <lga24@cornell.edu>
2017-11-20 17:40:54 -0500

    Finding similarities for step numbers

--- competition2.py
+++ competition2.py
@@ -1,9 +1,10 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-observations = np.genfromtxt("Observations.csv", delimiter = ',')
-labels = np.genfromtxt("Label.csv", delimiter=',')
-
+print 'Loading files...'
+observations = np.genfromtxt("../data/Observations.csv", delimiter = ',')
+labels = np.genfromtxt("../data/Label.csv", delimiter=',')
+print 'Done loading files'
 
 # Visualize the clustering on a 2D graph
 # Credit: Special thanks to Ilan Filonenko for this plotting approach
@@ -19,12 +20,27 @@ def visualizeClusters(M, clusters, title="Clustering"):
 	plt.show()
 
 def createLocationMatrix(labels):
+	print 'Creating location matrix...'
 	M = np.zeros((6000, 1000), dtype = 'float32, float32')
 
-	for i in range(labels.shape[0]):
-		row, column, x, y = labels[i, :]
-		M[row, column] = (x, y)
+	stepDict = {}
 
+	for i in range(labels.shape[0]):
+		run, step, x, y = labels[i, :]
+		run = int(run)
+		step = int(step)
+		if step not in stepDict:
+			stepDict[step] = {
+				'values': set(),
+				'count': 0
+			}
+		stepDict[step]['values'].add((x,y))
+		stepDict[step]['count'] += 1
+		M[run-1, step-1] = (x, y)
+
+	print 'Done creating location matrix'
+	for step, value in stepDict.items():
+		print 'Step: ', step, '|', len(value['values']), '/', value['count'], 'unique locs'
 	return M
 
 
@@ -32,9 +48,7 @@ def run():
 	M = createLocationMatrix(labels)
 
 
-
-
 if __name__ == '__main__':
 	np.set_printoptions(threshold=np.nan)
 	run()
-	runVisuals()
+	# runVisuals()
