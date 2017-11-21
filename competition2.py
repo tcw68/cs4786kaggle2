77a76f75b762399a41cceab50c5a2e23e9dcbdab
Annie Cheng <ac962@cornell.edu>
2017-11-20 20:14:55 -0500

    Fix preprocessing

--- competition2.py
+++ competition2.py
@@ -4,10 +4,6 @@ from hmmlearn import hmm
 import Markov
 
 
-observations = np.genfromtxt("Observations.csv", delimiter = ',')
-labels = np.genfromtxt("Label.csv", delimiter=',')
-
-
 # Visualize the clustering on a 2D graph
 # Credit: Special thanks to Ilan Filonenko for this plotting approach
 def visualizeClusters(M, clusters, title="Clustering"):
@@ -21,47 +17,44 @@ def visualizeClusters(M, clusters, title="Clustering"):
 	plt.colorbar()
 	plt.show()
 
-def createLocationMatrix(labels):
+# Create 6000 x 1000 location matrix from Labels.csv
+# M[i][j] = (x, y) --> At Run i + 1 and Step j + 1, bot is a location (x, y)
+def createLocationMatrix():
+	labels = np.genfromtxt("../Label.csv", delimiter=',')
 	M = np.zeros((6000, 1000), dtype = 'float32, float32')
 
 	for i in range(labels.shape[0]):
-		row, column, x, y = labels[i, :]
-		M[int(row), int(column)] = (x, y)
+		run, step, x, y = labels[i, :]
+		M[int(run-1), int(step-1)] = (x, y)
 
 	return M
 
-def createObservationMatrix(observations):
-	O = np.zeros((10000, 1000))
-	for i in range(observations.shape[0]):
-		for j in range(observations.shape[1]):
-			O[i, j] = observations[i, j]
+# Create observations matrix from Observations.csv
+# M[i][j] = Angle of bot to x-axis on Run i + 1 and Step j + 1
+def createObservationMatrix():
+	observations = np.genfromtxt("../Observations.csv", delimiter = ',')
 
-	return O
+	return observations 
 
 def guassianHMM():
 	model = hmm.GaussianHMM(n_components=3, covariance_type="full")
-
-def haha():
-	return
 
 def createSubmission(matrix):
 	N = np.zeros((12000, 1))
 
 	for i in range(matrix.shape[0]):
-		N[2i, :] = matrix[i, 0]
-		N[2i+1, :] = matrix[i, 1]
+		N[2*i, :] = matrix[i, 0]
+		N[2*i+1, :] = matrix[i, 1]
 
 	np.savetxt('hmm_submission.csv', N, fmt='%f', delimiter=",", header="Id,Label", comments='')
 
 
 def run():
-	M = createLocationMatrix(labels)
-	O = createObservationMatrix(observations)
-
-
+	LM = createLocationMatrix()
+	OM = createObservationMatrix()
 
 
 if __name__ == '__main__':
 	np.set_printoptions(threshold=np.nan)
 	run()
-	runVisuals()
+
