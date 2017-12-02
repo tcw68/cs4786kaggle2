6e63922d3f7d552ca41b85c01b2e4c449c0849b2
thomas wong <thomascwong1996@gmail.com>
2017-12-02 16:20:27 -0500

    fixed code a bit

--- svm.py
+++ svm.py
@@ -1,17 +1,19 @@
-from sklearn.svm import SVR
 from sklearn.externals import joblib
+from sklearn.svm import SVR
 import numpy as np
 
-def runSVR():
+# Followed this example from sklearn library using RBF kernel:
+# http://scikit-learn.org/stable/auto_examples/svm/plot_svm_regression.html#sphx-glr-auto-examples-svm-plot-svm-regression-py
+def runSVR(sampleSize, numPrevAngles):
 	labels = np.genfromtxt("../Label.csv", delimiter=',')
 	observations = np.genfromtxt("../Observations.csv", delimiter = ',')
 
 	angles, xs, ys = [], [], []
-	for label in labels[:10000]:
+	for label in labels[:sampleSize]:
 		run, step, x, y = label
-		run, step = int(run), int(step)
+		run, step = int(run) - 1, int(step) - 1
+		prevAngleSeq = observations[run, step-numPrevAngles:step]
 
-		prevAngleSeq = observations[run-1,step-31:step-1] # Previous angles
 		angles.append(prevAngleSeq)
 		xs.append(x)
 		ys.append(y)
@@ -22,42 +24,56 @@ def runSVR():
 
 	print "Run SVR to predict X locations"
 
-	xModel = SVR(kernel='rbf', C=1e3, gamma=0.1)
+	xModel = SVR(C=1e3, gamma=0.1)
 	xModel.fit(angles, xs)
+	joblib.dump(xModel, 'svr_xmodel_%i.pkl' % numPrevAngles) 
 
-	joblib.dump(xModel, 'svr_xmodel_last30angles.pkl') 
+	print "Run SVR to predict Y locations"
 
-	print "RUN SVR to predict Y locations"
-
-	yModel = SVR(kernel='rbf', C=1e3, gamma=0.1)
+	yModel = SVR(C=1e3, gamma=0.1)
 	yModel.fit(angles, ys)
-
-	joblib.dump(yModel, 'svr_ymodel_last30angles.pkl') 
+	joblib.dump(yModel, 'svr_ymodel_%i.pkl' % numPrevAngles) 
 
 # Create submission file using 4000 (x, y) predicted location points
 def createSubmission(predLocations, fileName):
-	print 'creating submission file...'
+	print 'Creating submission file...'
+
 	with open(fileName, 'wb') as f:
 		f.write('Id,Value\n')
 
 		for i, (x, y) in enumerate(predLocations):
-			xLine = ','.join([str(i+6001)+'x', str(x)])
-			yLine = ','.join([str(i+6001)+'y', str(y)])
+			xLine = ','.join([str(i + 6001) + 'x', str(x)])
+			yLine = ','.join([str(i + 6001) + 'y', str(y)])
 			f.write(xLine + '\n')
 			f.write(yLine + '\n')
 
-if __name__ == '__main__':
-	observations = np.genfromtxt("../Observations.csv", delimiter = ',')
-	data = observations[6000:,-30:]
+	print 'Done creating submission file...'
 
-	xModel = joblib.load('svr_xmodel_last30angles.pkl')
-	yModel = joblib.load('svr_ymodel_last30angles.pkl')
+if __name__ == '__main__':
+	sampleSize = 10000
+	numPrevAngles = 5
+
+	# Train SVR using labels
+
+	print "Start training SVR"
+	runSVR(sampleSize, numPrevAngles)
+	print "Done training SVR"
+
+	# Predict last 4000 1001th locations
+
+	print "Start predicting using SVR"
+	observations = np.genfromtxt("../Observations.csv", delimiter = ',')
+	data = observations[6000:,-numPrevAngles:]
+
+	xModel = joblib.load('svr_xmodel_%i.pkl' % numPrevAngles)
+	yModel = joblib.load('svr_ymodel_%i.pkl' % numPrevAngles)
 
 	xs = xModel.predict(data)
 	ys = yModel.predict(data)
 
 	predLocations = zip(xs, ys)
-	createSubmission(predLocations, 'svr_last30angles_submission.csv')
+	createSubmission(predLocations, 'svr_submission_%i.csv' % numPrevAngles)
+	print "Done predicting using SVR"
 
 
 
